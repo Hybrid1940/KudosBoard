0b3fe4cef5baa9c81efb8bccb1cde21280e6b6f9
Mahesh Andres Bachu <maheshbachu@hotmail.com>
2025-06-18 07:33:17 -0700

    This commit will initialize the CRUD for Boards

--- backEnd/routes/projectRoutes.js
+++ backEnd/routes/projectRoutes.js
@@ -3,11 +3,13 @@ const {PrismaClient} = require('@prisma/client');
 const router = express.Router();
 const prisma = new PrismaClient;
 
-router.get('/users', async (req, res) => {
+//CRUD for Boards
+//prints all boards
+router.get('/boards', async (req, res) => {
   const boards = await prisma.board.findMany()
   res.json(boards)
 })
-
+//prints 1 board
 router.get('/:boardId', async (req, res) => {
   const boardId = parseInt(req.params.boardId)
   console.log(boardId);
@@ -17,20 +19,33 @@ router.get('/:boardId', async (req, res) => {
   console.log(task);
   res.json(task);
 })
- 
+//deletes board
 router.delete('/:boardId', async (req, res) => {
   const boardId = parseInt(req.params.boardId)
-  console.log(boardId);
   const deletedBoard = await prisma.board.delete({
     where: { id: parseInt(boardId) }
   }); 
   console.log(deletedBoard);
   res.json(deletedBoard);
 })
-
-router.post('/users', async (req, res) => {
+//creates board
+router.post('/boards', async (req, res) => {
   const { name, category, Author} = req.body;
   const nBoard = await prisma.board.create({
+    data: {
+      name,
+      category, 
+      Author
+    }
+  });
+  res.json(nBoard);
+})
+//updates board
+router.put('/:boardId', async (req, res) => {
+  const id = parseInt(req.params.boardId);
+  const { name, category, Author} = req.body;
+  const nBoard = await prisma.board.update({
+    where: { id: parseInt(id) },
     data: {
       name,
       category, 
