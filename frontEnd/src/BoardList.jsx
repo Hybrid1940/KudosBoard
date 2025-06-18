34313889054fd214a7e37ca9f9e793d26059af22
Mahesh Andres Bachu <maheshbachu@hotmail.com>
2025-06-18 11:41:55 -0700

    Deploying updates

--- frontEnd/src/BoardList.jsx
+++ frontEnd/src/BoardList.jsx
@@ -4,16 +4,14 @@ import ReactDOM from "react-dom";
 import Board from "./Board";
 
 function BoardList() {
-  const [boards, setBoards] = useState(null);
+  const [boards, setBoards] = useState([]);
   useEffect(() => {
     fetch("http://localhost:3000/boards")
       .then((response) => response.json())
       .then((boards) => setBoards(boards))
       .catch((error) => console.error("Error fetching posts:", error));
   }, []);
-
   console.log(boards);
-
   return (
     <section
       style={{
@@ -21,12 +19,11 @@ function BoardList() {
         gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))",
       }}
     >
-      <Board />
-      <Board />
-      <Board />
-      <Board />
-      <Board />
-      <Board />
+      {boards.map((board) => {
+        return (
+          <Board name={board.name} category={board.category} id={board.id} />
+        );
+      })}
     </section>
   );
 }
